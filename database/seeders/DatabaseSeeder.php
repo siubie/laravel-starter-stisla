d043cc1898c8246aeaa57a0eb02a9c89980cf4e4
Evina08 <=>
2021-10-15 15:17:23 +0700

    install yajra n add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        User::create([
+            'name'=> "Dinda",
+            'email' => "dinda@gmail.com",
+            'password' => bcrypt('dinda123')
+        ]);
     }
 }
