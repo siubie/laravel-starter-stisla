801bbcdbb3742b465db5781dc298dbe28f2f151d
Evina08 <=>
2021-10-16 08:20:34 +0700

    seeding user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        User::create([
-            'name'=> "Dinda",
-            'email' => "dinda@gmail.com",
-            'password' => bcrypt('dinda123')
+        $this->call([
+            UserSeeder::class,
         ]);
     }
 }
