3353c2fc2ebef85ccb00b681c1048fe7916d36e4
Putra Prima Arhandi <putraprima@gmail.com>
2021-10-18 04:28:41 +0700

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         User::create([
-            'name'=> "Dinda",
+            'name' => "Dinda",
             'email' => "dinda@gmail.com",
             'password' => bcrypt('dinda123')
         ]);
