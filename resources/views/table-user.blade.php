36ca5ba23fd6e65ed4c845c913ead1ad75f18a27
jayamahendra <jayamahendra490@gmail.com>
2021-10-21 15:21:28 +0700

    href table-user ke halaman edit

--- resources/views/table-user.blade.php
+++ resources/views/table-user.blade.php
@@ -13,33 +13,34 @@
             </div>
         </div>
         <div class="section-body">
+            <h2 class="section-title">DataTables</h2>
             <div class="row">
-                <div class="col-12 ">
-                    <div class="row">
-                        <div class="col-lg-12 margin-tb">
-
-                            <div class="pull-right mb-2">
-                                <a class="btn btn-success" href="{{ route('user.create') }}"> Create New User</a>
-                            </div>
-                        </div>
-                    </div>
-                    @if ($message = Session::get('success'))
-                    <div class="alert alert-success">
-                        <p>{{ $message }}</p>
-                    </div>
-                    @endif
+                <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Table User</h4>
+                            <div class="col-lg-12 margin-tb">
+                                <div class="pull-right mb-2">
+                                    <a class="btn btn-success" href="{{ route('user.create') }}"> Create New User</a>
+                                </div>
+                            </div>
                         </div>
-                        <div class="card-body p-0">
+
+                        <div class="row">
+                        </div>
+                        @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                        @endif
+                        <div class="card-body">
                             <div class="table-responsive">
-                                <div class="container">
-                                    <table id="myTable" class="table table-stripped">
-                                        <thead>
+                                <table id="myTable" class="table table-stripped">
+                                    <thead>
+                                        <tr>
                                             <th>#</th>
                                             <th>Name</th>
                                             <th>Email</th>
+                                            <th>Created at</th>
                                             <th>Status</th>
                                             <th>Action</th>
                                         </tr>
@@ -54,13 +55,13 @@
                                             <td>
                                                 <div class="badge badge-success">Active</div>
                                             </td>
-                                            <td>
-                                                <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit" href="javascript:void(0)" onClick="editFunc('{{ $user->id }}')"><i class="fas fa-pencil-alt"></i></a>
-                                                <form class="delete-user" action="{{route('user.destroy',$user)}}" method="POST">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <a class=" btn btn-danger btn-action swal-6"  data-toggle="tooltip" data-id="{{ $user->id }}" title="Delete" onClick="return false"><i class="fas fa-trash"></i></a>
-                                                    </form>
+                                            <td class="d-flex align-items-center">
+                                                <a class="btn btn-primary btn-action mr-1" data-toggle="tooltip" title="Edit" href="{{route ('user.edit', $user->id)}}"><i class="fas fa-pencil-alt"></i></a>
+                                                <form class="delete-user ml-2" action="{{route('user.destroy',$user)}}" method="POST" >
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <a class=" btn btn-danger btn-action swal-6" data-toggle="tooltip" data-id="{{ $user->id }}" title="Delete" onClick="return false"><i class="fas fa-trash"></i></a>
+                                                </form>
                                             </td>
                                         </tr>
                                         @endforeach
@@ -74,107 +75,5 @@
             </div>
         </div>
     </section>
-</div>
-
-<!-- Create new User -->
-<div class="modal fade" id="user-modal" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="UserModal"></h4>
-                <button type="button" class="close modelClose" data-dismiss="modal">&times;</button>
-            </div>
-            <div class="modal-body">
-                <form action="javascript:void(0)" id="UserForm" name="UserForm" class="form-horizontal" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <input type="hidden" name="id" id="id">
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">User Name</label>
-                        <div class="col-sm-12">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter User Name" maxlength="50" required="">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">User Email</label>
-                        <div class="col-sm-12">
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Enter User Email" maxlength="50" required="">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-2 control-label">User Password</label>
-                        <div class="col-sm-12">
-                            <input type="text" class="form-control" id="password" name="password" placeholder="Enter Password" required="">
-                        </div>
-                    </div>
-                    <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-primary" id="btn-save">Save changes
-                        </button>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Edit User -->
-<div class="modal fade" id="user-edit" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="UserEdit"></h4>
-                <button type="button" class="close modelClose" data-dismiss="modal">&times;</button>
-            </div>
-            <div class="modal-body">
-                <form action="javascript:void(0)" id="UserFormEdit" name="UserForm" class="form-horizontal" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <input type="hidden" name="id" id="idtext">
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">User Name</label>
-                        <div class="col-sm-12">
-                            <input type="text" class="form-control" id="nametext" name="name" placeholder="Enter User Name" maxlength="50" required="">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="name" class="col-sm-2 control-label">User Email</label>
-                        <div class="col-sm-12">
-                            <input type="email" class="form-control" id="emailtext" name="email" placeholder="Enter User Email" maxlength="50" required="">
-                            <input type="hidden" class="form-control" id="passwordtext" name="password" placeholder="Enter User Email" maxlength="50" required="">
-                        </div>
-                    </div>
-
-                    <div class="col-sm-offset-2 col-sm-10">
-                        <button type="submit" class="btn btn-primary" id="btn-save">Save changes
-                        </button>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- Delete User -->
-<div class="modal" id="DeleteUserModal">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <!-- Modal Header -->
-            <div class="modal-header">
-                <h4 class="modal-title">User Delete</h4>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-            </div>
-            <!-- Modal body -->
-            <div class="modal-body">
-                <h4>Are you sure want to delete this User?</h4>
-            </div>
-            <!-- Modal footer -->
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" id="SubmitDeleteUserForm">Yes</button>
-                <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
