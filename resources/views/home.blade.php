39fae6e30b7f2426cc300317efbf9446f28c9d60
Putra Prima Arhandi <putraprima@gmail.com>
2021-10-25 05:22:08 +0700

    Fixing Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 @section('content')
-    <div class="main-content">
-        <section class="section">
-            <div class="section-header">
-                <h1>Blank Page</h1>
+    <section class="section">
+        <div class="section-header">
+            <h1>Table</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                <div class="breadcrumb-item"><a href="#">Components</a></div>
+                <div class="breadcrumb-item">Table</div>
             </div>
-
-            <div class="section-body">
-            </div>
-        </section>
-    </div>
+        </div>
+        <div class="section-body">
+        </div>
+    </section>
 @endsection
