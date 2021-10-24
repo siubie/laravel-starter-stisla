141c1d1f00f88a4abd8f91886865f0e87f8e1f36
Putra Prima Arhandi <putraprima@gmail.com>
2021-10-24 09:58:58 +0700

    Fixing UI

--- resources/views/create-user.blade.php
+++ resources/views/create-user.blade.php
@@ -1,61 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Form Create User</h1>
-            <div class="section-header-breadcrumb">
-                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="#">Forms</a></div>
-                <div class="breadcrumb-item">Form Create User</div>
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Form Create User</h1>
+                <div class="section-header-breadcrumb">
+                    <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+                    <div class="breadcrumb-item"><a href="#">Forms</a></div>
+                    <div class="breadcrumb-item">Form Create User</div>
+                </div>
             </div>
-        </div>
 
-        <div class="section-body">
-            <h2 class="section-title">Tambah User</h2>
+            <div class="section-body">
+                <h2 class="section-title">Tambah User</h2>
 
-            <div class="card">
-                <form action="{{ route('user.store') }}" method="post">
-                    <div class="card-header">
-                        <h4>Validasi Tambah Data</h4>
-                    </div>
-                    <div class="card-body">
-                        @csrf
-                        <div class="form-group">
-                            <label for="name">Your Name</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="Enter User Name">
-                            @error('name')
-                            <div class="invalid-feedback">
-                                {{ $message }}
-                            </div>
-                            @enderror
+                <div class="card">
+                    <form action="{{ route('user.store') }}" method="post">
+                        <div class="card-header">
+                            <h4>Validasi Tambah Data</h4>
                         </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Enter User Email" value="{{ old('email') }}">
-                            @error('email')
-                            <div class="invalid-feedback">
-                                {{ $message }}
+                        <div class="card-body">
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Your Name</label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                                    name="name" placeholder="Enter User Name">
+                                @error('name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" placeholder="Enter User Password">
-                            @error('password')
-                            <div class="invalid-feedback">
-                                {{ $message }}
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email"
+                                    name="email" placeholder="Enter User Email" value="{{ old('email') }}">
+                                @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
-                            @enderror
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                    id="password" name="password" placeholder="Enter User Password">
+                                @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-footer text-right">
-                        <button class="btn btn-primary">Submit</button>
-                    </div>
-                </form>
+                        <div class="card-footer text-right">
+                            <button class="btn btn-primary">Submit</button>
+                            <a class="btn btn-secondary" href="{{ route('user.index') }}">Cancel</a>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-    </section>
-</div>
+        </section>
+    </div>
 @endsection
