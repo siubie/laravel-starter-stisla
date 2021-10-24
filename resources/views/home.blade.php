94becf6a7903863bbd8f812344a818b26db26f96
Putra Prima Arhandi <putraprima@gmail.com>
2021-10-24 11:21:33 +0700

    Fixing Title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('content')
-<div class="main-content">
-    <section class="section">
-        <div class="section-header">
-            <h1>Blank Page</h1>
-        </div>
+    <div class="main-content">
+        <section class="section">
+            <div class="section-header">
+                <h1>Blank Page</h1>
+            </div>
 
-        <div class="section-body">
-        </div>
-    </section>
-</div>
+            <div class="section-body">
+            </div>
+        </section>
+    </div>
 @endsection
