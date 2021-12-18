1630fc34324bd84bf4a1f732c4087a1cb7cf161a
Putra Prima Arhandi <putraprima@gmail.com>
2021-12-18 07:05:13 +0700

    [Add] Start Roles and Permission Controller

---
+++ app/Http/Controllers/Roles.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Roles extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Roles  $roles
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Roles $roles)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Roles  $roles
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Roles $roles)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Roles  $roles
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Roles $roles)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Roles  $roles
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Roles $roles)
+    {
+        //
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,9 @@
         <div class="row">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header"></div>
+                    <div class="card-header">
+                        <h4>Title</h4>
+                    </div>
                     <div class="card-body"></div>
                 </div>
             </div>
