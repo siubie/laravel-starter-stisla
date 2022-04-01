c24497414f1e0f41d78ef640f8d50bd0a2b53ba9
siubie <putraprima@gmail.com>
2022-04-02 06:13:13 +0700

    Move Categories out of sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::apiResource('categories', CategoryController::class);
+    // Route::apiResource('categories', CategoryController::class);
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
+Route::apiResource('categories', CategoryController::class);
